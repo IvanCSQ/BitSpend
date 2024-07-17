200fa3616320f1f65f098aaa1f7826d9dcd6a0ba
ZhiCong Ore <zhicong1989@gmail.com>
2024-07-17 16:41:49 +0800

    display categories and categories expenses amount in view

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,4 @@
 class ExpensesController < ApplicationController
   def analysis
-    @total = current_user.total_expenses_amount
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :expenses
+
+  def expenses_amount
+    total = 0
+
+    expenses.each do |expense|
+      total += expense.amount
+    end
+
+    total
+  end
 end
