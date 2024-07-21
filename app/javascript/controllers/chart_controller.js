9399580e410a6080f5907cf5dfcb34cb5452095d
ZhiCong Ore <zhicong1989@gmail.com>
2024-07-22 00:08:38 +0800

    display chart according to user categories

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -3,23 +3,29 @@ import { Chart, registerables } from "chart.js";
 
 // Connects to data-controller="chart"
 export default class extends Controller {
+  static values = {
+    categoryList: Array,
+    categoryAmount: Object
+  };
+
   connect() {
     Chart.register(...registerables)
 
+    const userCategories = this.categoryListValue.map((category) => category.name)
+
     const data = {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-      ],
+      labels: userCategories,
+      legend: {
+        display: false
+      },
       datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
+        // label: 'My First Dataset',
+        data: Object.values(this.categoryAmountValue),
+        // backgroundColor: [
+        //   'rgb(255, 99, 132)',
+        //   'rgb(54, 162, 235)',
+        //   'rgb(255, 205, 86)'
+        // ],
         hoverOffset: 4
       }]
     };
