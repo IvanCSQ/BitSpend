f091cb7cb1e1e7e289bf050b212e647f5e1c8fc2
ZhiCong Ore <zhicong1989@gmail.com>
2024-07-22 21:13:56 +0800

    display category labels and percentage for chart

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import { Chart, registerables } from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 // Connects to data-controller="chart"
 export default class extends Controller {
@@ -9,15 +10,12 @@ export default class extends Controller {
   };
 
   connect() {
-    Chart.register(...registerables)
+    Chart.register(...registerables, ChartDataLabels);
 
     const userCategories = this.categoryListValue.map((category) => category.name)
 
     const data = {
       labels: userCategories,
-      legend: {
-        display: false
-      },
       datasets: [{
         // label: 'My First Dataset',
         data: Object.values(this.categoryAmountValue),
@@ -30,13 +28,46 @@ export default class extends Controller {
       }]
     };
 
+    const options = {
+      plugins: {
+        legend: {
+          display: false
+        },
+        datalabels: {
+          formatter: (value, context) => {
+            // Calculate percentage from value and total
+            const total = context.chart.data.datasets[0].data.reduce((acc, val) => acc + val, 0);
+            const percentage = ((value / total) * 100).toFixed(2) + '%';
+
+            // Return label and percentage
+            return `${percentage}\n${context.chart.data.labels[context.dataIndex]}`;
+          },
+          anchor: 'start',
+          align: 'end',
+          offset: 80,
+          display: 'hidden',
+          rotation: 0
+        }
+      },
+      layout: {
+        padding: {
+          left: 70,
+          right: 70,
+          top: 70,
+          bottom: 70
+        }
+      }
+    }
+
     const config = {
       type: 'pie',
-      data: data,
+      data,
+      options
     };
 
     const chart = new Chart(
-      this.element, config
+      this.element,
+      config
     );
   }
 }
