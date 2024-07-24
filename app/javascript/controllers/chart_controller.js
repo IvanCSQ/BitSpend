1f5dce0679b81ac534159c626c3503dd1a1fee38
ZhiCong Ore <zhicong1989@gmail.com>
2024-07-24 12:38:34 +0800

    remove bootstrap

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -17,13 +17,7 @@ export default class extends Controller {
     const data = {
       labels: userCategories,
       datasets: [{
-        // label: 'My First Dataset',
         data: Object.values(this.categoryAmountValue),
-        // backgroundColor: [
-        //   'rgb(255, 99, 132)',
-        //   'rgb(54, 162, 235)',
-        //   'rgb(255, 205, 86)'
-        // ],
         hoverOffset: 4
       }]
     };
