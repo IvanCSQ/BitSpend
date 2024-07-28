22753809b89c2da818a26539d258c17431488da3
ZhiCong Ore <zhicong1989@gmail.com>
2024-07-28 17:10:02 +0800

    format pie chart labels to rotation with respect to pie angle

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -12,8 +12,10 @@ export default class extends Controller {
   connect() {
     Chart.register(...registerables, ChartDataLabels);
 
+    // List of user's categories to be used for chart labels and legends
     const userCategories = this.categoryListValue.map((category) => category.name.toLowerCase())
 
+    // Categories and total expenses amount per category formatting for chart data
     const data = {
       labels: userCategories,
       datasets: [{
@@ -22,6 +24,7 @@ export default class extends Controller {
       }]
     };
 
+    // Chart display options
     const options = {
       plugins: {
         legend: {
@@ -29,19 +32,19 @@ export default class extends Controller {
           labels: {
             font: {
               family: '"Press Start 2P", "Helvetica", "sans-serif"',
-              size: 12,
+              size: 12
             },
             boxWidth: 20
           },
           align: 'start'
         },
+        // Labels display in pie chart
         datalabels: {
           formatter: (value, context) => {
-            // Calculate percentage from value and total
+            // Calculate user's total expenses amount and convert category expenses amount into percentage of total amount
             const total = context.chart.data.datasets[0].data.reduce((acc, val) => acc + val, 0);
             const percentage = ((value / total) * 100).toFixed(1) + '%';
 
-            // Return label and percentage
             return percentage;
           },
           anchor: 'end',
@@ -50,17 +53,40 @@ export default class extends Controller {
             size: 12,
             family: '"Press Start 2P", "Helvetica", "sans-serif"'
           },
-          textAlign: 'end',
           rotation: function(context) {
-            console.log(context.chart.data.datasets[0].data);
             const total = context.chart.data.datasets[0].data.reduce((acc, val) => acc + val, 0);
-            let angles = []
 
-            context.chart.data.datasets[0].data.map((amount) => {
+            // Create an object with index as key and category expenses amount as value
+            let amountPerCategory = {}
 
-            })
+            for (let counter = 0; counter < context.chart.data.datasets[0].data.length; counter++) {
+              amountPerCategory[counter] = context.dataset.data[counter]
+            }
 
-            return angles
+            // Create an object with index: angle of each category pie
+            let pieAngles = {}
+
+            for (let counter = 0; counter < context.chart.data.datasets[0].data.length; counter++) {
+              pieAngles[counter] = Math.round(amountPerCategory[counter] / total * 360);
+            }
+
+            // Create an object with index: starting angle of each category pie
+            let pieStartingAngles = {0: 0}
+
+            for (let counter = 1; counter < context.chart.data.datasets[0].data.length; counter++) {
+              pieStartingAngles[counter] = pieAngles[counter - 1] + pieStartingAngles[counter - 1];
+            }
+
+            // Calculate the rotation angle of each category pie
+            let rotationAngles = []
+
+            for (let counter = 0; counter < context.chart.data.datasets[0].data.length; counter++) {
+              // const initialRotation = pieStartingAngles[counter] + (pieAngles[counter] / 2)
+              // initialRotation > 180 ? rotationAngles.push(pieStartingAngles[counter] + (pieAngles[counter] / 2) + 90) : rotationAngles.push(pieStartingAngles[counter] + (pieAngles[counter] / 2) - 90)
+              rotationAngles.push(pieStartingAngles[counter] + (pieAngles[counter] / 2) - 90)
+            }
+
+            return rotationAngles
           }
         }
       },
